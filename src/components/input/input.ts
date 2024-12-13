b51736e814672faef3937d7cdce4f1d89be9b384
Arsen Tamazyan <atamazyan@nota.tech>
2024-12-13 11:06:38 +0300

    test(input): протестировать работу сигналов для input
    сигалы прекрасно работают
    
    несколько сигналов для одного эффекта тоже корректно работают
    
    сигналы между элементами не пересекаются
    
    одновременное изменение нескольких сигналов тоже работает

--- src/components/input/input.ts
+++ src/components/input/input.ts
@@ -1,19 +1,31 @@
 import { ComponentConfig } from "@shared/types";
-import { BaseElement, component, createElement, property, signal } from "@shared/utils";
+import { BaseElement, component, createElement, effect, property, signal } from "@shared/utils";
 
 @component('rx-input')
 export class InputElement extends BaseElement {
   @property()
   placeholder = signal('')
 
+  @property()
+  test = signal('')
+
+  constructor() {
+    super()
+    console.log('signal created', this.placeholder)
+  }
+
   rootStyle?: Promise<typeof import("*?raw")> | undefined = import('./input.scss?raw');
 
   render(): ComponentConfig {
+    const inputEl = createElement('input')
+    effect(() => {
+      inputEl.setAttribute('placeholder', this.placeholder())
+      inputEl.setAttribute('value', this.test())
+    })
     return createElement('div')
       .addClass('container')
       .append(
-        createElement('input')
-          .addClass('input-field')
+        inputEl.addClass('input-field')
       )
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <p>веб-компоненты</p>
     <div class="component-wrapper">
+      <rx-input id="inp1"></rx-input>
       <rx-input></rx-input>
       <rx-input></rx-input>
       <rx-button></rx-button>
