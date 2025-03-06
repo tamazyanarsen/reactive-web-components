6c44fbf979e1c430cbeec9df9e1e9cb12e358d6d
Arsen Tamazyan <tamazyanarsen@mail.ru>
2025-03-06 07:05:54 +0300

    feat(select): закрытие dropdown после выбора элемента

--- src/components/dropdown/dropdown.ts
+++ src/components/dropdown/dropdown.ts
@@ -1,62 +1,76 @@
 import { ComponentConfig, ListItem, SelectedItem } from "@shared/types";
-import { BaseElement, component, createElement, event, newEventEmitter, property, signal } from "@shared/utils";
+import {
+    BaseElement,
+    component,
+    createElement,
+    event,
+    newEventEmitter,
+    property,
+    signal,
+} from "@shared/utils";
 
-@component('rx-dropdown')
+@component("rx-dropdown")
 export class DropdownComponent extends BaseElement {
-  rootStyle?: Promise<typeof import("*?raw")> | undefined = import('./dropdown.scss?raw');
+    rootStyle?: Promise<typeof import("*?raw")> | undefined = import(
+        "./dropdown.scss?raw"
+    );
 
-  @property()
-  items = signal<ListItem[]>([])
+    @property()
+    items = signal<ListItem[]>([]);
 
-  @event()
-  selected = newEventEmitter<SelectedItem>()
+    @event()
+    selected = newEventEmitter<SelectedItem>();
 
-  selectedItems = signal<SelectedItem>({})
+    selectedItems = signal<SelectedItem>({});
 
-  @property()
-  isMultiSelect = signal(true)
+    @property()
+    isMultiSelect = signal(true);
 
-  render(): ComponentConfig {
-    return createElement('div')
-      .addClass('items-container')
-      .addEffect(self => {
-        console.log('DROPDOWN FIRST EFFECT');
-        this.selectedItems = signal<SelectedItem>(this.selectedItems())
-        self.set(
-          ...this.items()
-            .map(e => {
-              return createElement('div')
-                .addClass('item')
-                .addEffect(self => {
-                  console.log('DROPDOWN SECOND EFFECT');
-                  const selectedItems = this.selectedItems()
-                  if (e.value in selectedItems) self.addClass('selected')
-                  else self.removeClass('selected')
-                })
-                .setHtmlContent(e.label)
-                .addEventlistener('click', ev => {
-                  ev.stopPropagation();
-                  this.selectedItems.update(oldSelected => {
-                    if (e.value in oldSelected) delete oldSelected[e.value]
-                    else {
-                      if (!this.isMultiSelect()) oldSelected = {}
-                      oldSelected[e.value] = e.label
-                    }
-                    if (this.items().length < 10) return oldSelected
-                    this.items.update(oldItems => {
-                      const selected = this.selectedItems()
-                      oldItems.sort(
-                        (a, b) =>
-                          +!!selected[b.value] - +!!selected[a.value]
-                      );
-                      return oldItems
-                    })
-                    return oldSelected
-                  });
-                  this.selected(this.selectedItems())
-                })
-            })
-        )
-      })
-  }
+    render(): ComponentConfig {
+        return createElement("div")
+            .addClass("items-container")
+            .addEffect((self) => {
+                console.log("DROPDOWN FIRST EFFECT");
+                this.selectedItems = signal<SelectedItem>(this.selectedItems());
+                self.set(
+                    ...this.items().map((e) => {
+                        return createElement("div")
+                            .addClass("item")
+                            .addEffect((self) => {
+                                console.log("DROPDOWN SECOND EFFECT");
+                                const selectedItems = this.selectedItems();
+                                if (e.value in selectedItems)
+                                    self.addClass("selected");
+                                else self.removeClass("selected");
+                            })
+                            .setHtmlContent(e.label)
+                            .addEventlistener("click", (ev) => {
+                                ev.stopPropagation();
+                                this.selectedItems.update((oldSelected) => {
+                                    if (e.value in oldSelected)
+                                        delete oldSelected[e.value];
+                                    else {
+                                        if (!this.isMultiSelect())
+                                            oldSelected = {};
+                                        oldSelected[e.value] = e.label;
+                                    }
+                                    if (this.items().length < 10)
+                                        return oldSelected;
+                                    this.items.update((oldItems) => {
+                                        const selected = this.selectedItems();
+                                        oldItems.sort(
+                                            (a, b) =>
+                                                +!!selected[b.value] -
+                                                +!!selected[a.value],
+                                        );
+                                        return oldItems;
+                                    });
+                                    return oldSelected;
+                                });
+                                this.selected(this.selectedItems());
+                            });
+                    }),
+                );
+            });
+    }
 }
--- src/components/select/custom-select/select.ts
+++ src/components/select/custom-select/select.ts
@@ -1,54 +1,71 @@
 import { ComponentConfig, ListItem, SelectedItem } from "@shared/types";
-import { BaseElement, component, createCustomElement, createElement, property, signal } from "@shared/utils";
+import {
+  BaseElement,
+  component,
+  createCustomElement,
+  createElement,
+  property,
+  signal,
+} from "@shared/utils";
 import { DropdownComponent } from "../../dropdown/dropdown";
 
-@component('rx-select')
+@component("rx-select")
 export class SelectComponent extends BaseElement {
   @property()
-  items = signal<ListItem[]>([])
+  items = signal<ListItem[]>([]);
 
   @property()
-  isMulti = signal(true)
+  isMulti = signal(true);
 
-  selectedItems = signal<SelectedItem>({})
+  selectedItems = signal<SelectedItem>({});
 
-  isDropdownVisible = signal(false)
+  isDropdownVisible = signal(false);
 
-  rootStyle?: Promise<typeof import("*?raw")> | undefined = import('./select.scss?raw')
+  rootStyle?: Promise<typeof import("*?raw")> | undefined = import(
+    "./select.scss?raw"
+  );
 
   render(): ComponentConfig {
-    document.body.addEventListener('click', () => this.isDropdownVisible.set(false))
-    const dropdownEl = createCustomElement<DropdownComponent>('rx-dropdown')
-      .addClass('dropdown')
-      .setReactiveAttribute('isMultiSelect', this.isMulti)
-      .setReactiveAttribute('items', this.items)
-      .addEventlistener('selected', e => this.selectedItems.set(e.detail))
-    const inputEl = createElement('div')
-      .addClass('selected-container')
-      .addEffect(self => {
-        self.set(
-          ...Object.keys(this.selectedItems()).map(
-            itemValue => createElement('div')
-              .addClass('selected-item')
-              .addReactiveClass({ 'selected-item-max-width': this.isMulti })
-              .setHtmlContent(this.selectedItems()[itemValue])
-          )
-        )
-      })
-      .addEventlistener('click', ev => {
-        ev.stopPropagation();
-        this.isDropdownVisible.set(!this.isDropdownVisible())
-      });
-    return createElement('div')
-      .addClass('wrapper')
-      .append(inputEl)
-      .addEffect(self => {
-        if (this.isDropdownVisible()) {
-          dropdownEl.hostElement.style.width = inputEl.hostElement.getClientRects().item(0)?.width + 'px';
-          self.append(dropdownEl)
-        } else {
-          self.removeChild(dropdownEl)
+    document.body.addEventListener("click", () =>
+      this.isDropdownVisible.set(false),
+    );
+    const dropdownEl = createCustomElement<DropdownComponent>("rx-dropdown")
+      .addClass("dropdown")
+      .setReactiveAttribute("isMultiSelect", this.isMulti)
+      .setReactiveAttribute("items", this.items)
+      .addEventlistener("selected", (e) => {
+        if (!this.isMulti()) {
+          this.isDropdownVisible.set(false);
         }
+        this.selectedItems.set(e.detail);
+      });
+    const inputEl = createElement("div")
+      .addClass("selected-container")
+      .addEffect((self) => {
+        self.set(
+          ...Object.keys(this.selectedItems()).map((itemValue) =>
+            createElement("div")
+              .addClass("selected-item")
+              .addReactiveClass({ "selected-item-max-width": this.isMulti })
+              .setHtmlContent(this.selectedItems()[itemValue]),
+          ),
+        );
       })
+      .addEventlistener("click", (ev) => {
+        ev.stopPropagation();
+        this.isDropdownVisible.set(!this.isDropdownVisible());
+      });
+    return createElement("div")
+      .addClass("wrapper")
+      .append(inputEl)
+      .addEffect((self) => {
+        if (this.isDropdownVisible()) {
+          dropdownEl.hostElement.style.width =
+            inputEl.hostElement.getClientRects().item(0)?.width + "px";
+          self.append(dropdownEl);
+        } else {
+          self.removeChild(dropdownEl);
+        }
+      });
   }
 }
