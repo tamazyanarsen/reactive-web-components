6a648524f4c8c60dd0aa55a5538665d166735479
Arsen Tamazyan <atamazyan@nota.tech>
2025-03-15 19:20:23 +0300

    refactor

--- src/shared/utils/html-elements/component.ts
+++ src/shared/utils/html-elements/component.ts
@@ -3,7 +3,7 @@ import { createCustomEl } from "./custom-element"
 import { createEl } from "./element"
 
 export const createComponent = <K extends HtmlTagName>(srcComp: `${K} ${string}`):
-    K extends `rx-${string}` ? ReturnType<typeof createCustomEl<K, HTMLElementTagNameMap[K]>> : ReturnType<typeof createEl<K>> =>
+    ReturnType<K extends `rx-${string}` ? typeof createCustomEl<K, HTMLElementTagNameMap[K]> : typeof createEl<K>> =>
     srcComp.startsWith('rx-')
         ? createCustomEl(srcComp) as any
         : createEl(srcComp) as any
