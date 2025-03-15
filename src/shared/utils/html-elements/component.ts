74a46d0ff795f9e41bf5084dc0e5c6116e595e38
Arsen Tamazyan <atamazyan@nota.tech>
2025-03-15 18:21:30 +0300

    fix: типизация для универсального компонента

--- src/shared/utils/html-elements/component.ts
+++ src/shared/utils/html-elements/component.ts
@@ -2,6 +2,8 @@ import { HtmlTagName } from "@shared/types"
 import { createCustomEl } from "./custom-element"
 import { createEl } from "./element"
 
-export const createComponent = <K extends HtmlTagName>(srcComp: `${K} ${string}`) => srcComp.startsWith('rx-')
-    ? createCustomEl(srcComp)
-    : createEl(srcComp)
+export const createComponent = <K extends HtmlTagName>(srcComp: `${K} ${string}`):
+    K extends `rx-${string}` ? ReturnType<typeof createCustomEl<K, HTMLElementTagNameMap[K]>> : ReturnType<typeof createEl<K>> =>
+    srcComp.startsWith('rx-')
+        ? createCustomEl(srcComp) as any
+        : createEl(srcComp) as any
