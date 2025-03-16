cd9c86c2dd96403b414d1d24b03be47940c3cdb9
Arsen Tamazyan <atamazyan@nota.tech>
2025-03-16 14:58:18 +0300

    fix

--- src/shared/utils/html-elements/component.ts
+++ src/shared/utils/html-elements/component.ts
@@ -1,9 +1,10 @@
-import { HtmlTagName } from "@shared/types"
+import { ComponentInitConfig, HtmlTagName } from "@shared/types"
 import { createCustomHtmlElement } from "./custom-element"
 import { createEl } from "./element"
 
-export const createTemplate = <K extends HtmlTagName>(srcComp: `${K} ${string}`):
+export const createTemplate = <K extends HtmlTagName>(srcComp: `${K} ${string}`,
+    config?: ComponentInitConfig<HTMLElementTagNameMap[K]>):
     ReturnType<K extends `rx-${string}` ? typeof createCustomHtmlElement<K, HTMLElementTagNameMap[K]> : typeof createEl<K>> =>
     srcComp.startsWith('rx-')
-        ? createCustomHtmlElement(srcComp) as any
-        : createEl(srcComp) as any
+        ? createCustomHtmlElement(srcComp, config) as any
+        : createEl(srcComp, config) as any
